6bade86d2f682473c8e06dc8a4402331c4b24fd0
lekalekss <155861509+lekalekss@users.noreply.github.com>
2024-02-23 21:15:37 -0600

    new file

---
+++ Phone/Phone/Phone.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone
+{
+    public class Phone
+    {
+        public Phone() 
+        {
+            
+            Color = "black";
+            Price = 100;
+                    
+        }
+
+        public Phone(string operationSystem, string color, int price, string brand)
+        {
+            OperationSystem = operationSystem;
+
+            Color = color;
+
+            Price = price;
+
+            Brand = brand;
+        
+        }
+
+        public string OperationSystem { get; set; }
+
+        public string Color { get; set; }
+
+        private int Price { get; set; }
+
+        public string Brand { get; set; }
+
+        
+
+        public void printPrice () 
+        {
+            Console.WriteLine("The price is " + Price);
+        }
+
+        public int localTax { get; set; } = 13;
+        
+
+        
+        public void PhoneBoot ()
+        {
+            Console.WriteLine(OperationSystem +" " + Color+" " + Price+" " + Brand);
+        }
+
+        public int GetFullPrice ()
+        {            
+            int fullLocalPrice = Price * localTax / 100 + Price;
+            
+            return fullLocalPrice;
+        }
+
+        public int SetPhonePrice(int newPrice)
+        {
+            Price = newPrice;
+            return Price;
+        }
+
+        public void SetProvinceTax (string province)
+
+        {
+            switch (province)
+            {
+                case "MB":
+                    localTax = 13;
+                    break;
+
+                case "ON":
+                    localTax = 12;
+                    break;
+                case "SK":
+                    localTax = 10;
+                    break;
+                case "AB":
+                    localTax = 8;
+                    break;
+                case "BC":
+                    localTax = 11;
+                    break;
+
+                default:
+                    Console.WriteLine("Enter correct province");
+                    break;
+
+            }          
+
+        }
+
+    }
+    
+}
---
+++ Phone/Phone/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Phone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone phone = new Phone();
+
+            phone.printPrice();
+            Console.WriteLine("We have a phone with price " + "and color is " + phone.Color);
+
+
+
+            Phone phone2 = new Phone("iOS", "SilverGrey", 1560, "iPhone");
+
+            Console.WriteLine("\nWe are selling new phone");
+
+            Console.WriteLine("Operation system is " + phone2.OperationSystem);
+
+            Console.WriteLine("Color is " + phone2.Color);
+
+            phone2.printPrice();
+
+            Console.WriteLine("Brand is " + phone2.Brand);
+
+            phone2.PhoneBoot();
+
+            int newPrice = phone2.SetPhonePrice(2000);
+
+            Console.WriteLine("New price is " + newPrice);
+
+            int priceWithLocalTax = phone2.GetFullPrice();
+
+            Console.WriteLine("Price with local tax is " + priceWithLocalTax);
+
+            Phone sony = new Phone();
+            sony.SetPhonePrice(1000);
+            sony.SetProvinceTax("ON");
+            int ontarioPrice = sony.GetFullPrice();
+
+
+            Console.WriteLine("the price for the phone in Ontario is " + ontarioPrice);
+
+
+
+
+        }
+    }
+}
